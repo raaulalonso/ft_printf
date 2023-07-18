978ec87228698d5ec0146723a6bdd25eb1b63e1a
Raul Alonso Pardo <raalonso@c1r13s1.42madrid.com>
2023-07-18 15:06:54 +0200

    commit

--- ft_printf.c
+++ ft_printf.c
@@ -6,30 +6,29 @@
 /*   By: raalonso <raalonso@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/06 23:36:19 by raalonso          #+#    #+#             */
-/*   Updated: 2023/07/18 13:36:30 by raalonso         ###   ########.fr       */
+/*   Updated: 2023/07/18 15:03:49 by raalonso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-void	convers(char *print, int i, va_list param, int *count)
+void	convers(char *print, int i, va_list *param, int *count)
 {
-	if ((print[i] != '\0') && (print[i + 1] == 's'))
-		*count += (ft_putstr_fd(va_arg(param, char *), 1) - 2);
-	else if ((print[i] != '\0') && (print[i + 1] == 'c'))
-		*count += (ft_putchar_fd(va_arg(param, int), 1) - 2);
-	else if ((print[i] != '\0') && ((print[i + 1] == 'i')
-			|| (print[i + 1] == 'd')))
-		*count += (ft_putnbr_fd(va_arg(param, int), 1) - 2);
-	else if ((print[i] != '\0') && (print[i + 1] == 'u'))
-		*count += (ft_putnbr_unsigned(va_arg(param, unsigned int)) - 2);
-	else if ((print[i] != '\0') && (print[i + 1] == 'p'))
-		*count += (ft_putmem(va_arg(param, void *)) - 2);
-	else if ((print[i] != '\0') && (print[i + 1] == 'x'))
-		*count += (ft_puthex(va_arg(param, unsigned int), 1) - 2);
-	else if ((print[i] != '\0') && (print[i + 1] == 'X'))
-		*count += (ft_puthex(va_arg(param, unsigned int), 2) - 2);
-	else if ((print[i] != '\0') && (print[i + 1] == '%'))
+	if (print[i + 1] == 's')
+		*count += (ft_putstr_fd(va_arg(*param, char *), 1) - 2);
+	else if (print[i + 1] == 'c')
+		*count += (ft_putchar_fd(va_arg(*param, int), 1) - 2);
+	else if ((print[i + 1] == 'i') || (print[i + 1] == 'd'))
+		*count += (ft_putnbr_fd(va_arg(*param, int), 1) - 2);
+	else if (print[i + 1] == 'u')
+		*count += (ft_putnbr_unsigned(va_arg(*param, unsigned int)) - 2);
+	else if (print[i + 1] == 'p')
+		*count += (ft_putmem(va_arg(*param, void *)) - 2);
+	else if (print[i + 1] == 'x')
+		*count += (ft_puthex(va_arg(*param, unsigned int), 1) - 2);
+	else if (print[i + 1] == 'X')
+		*count += (ft_puthex(va_arg(*param, unsigned int), 2) - 2);
+	else if (print[i + 1] == '%')
 		*count -= ft_putchar_fd('%', 1);
 }
 
@@ -51,7 +50,7 @@ int	ft_printf(char const *print, ...)
 		}
 		if (print[i] == '\0')
 			break ;
-		convers((char *)print, i, param, &count);
+		convers((char *)print, i, &param, &count);
 		i += 2;
 	}
 	va_end(param);
